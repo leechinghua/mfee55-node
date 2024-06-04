e4d57f6c7dfd9a461d581e059f0a39008250a112
leechinghua <leechenghua@gmail.com>
2024-06-04 11:40:26 +0800

    第一支 express

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+
+import express from "express";
+
+const app = express();
+
+// 路由設定, routes
+// 1. get(): 只接受 HTTP GET 方法的拜訪
+// 2. 只接受 路徑為 / 的 request
+app.get("/", (req, res) => {
+  res.send("<h2>Hello World</h2>");
+});
+
+app.listen(3000, ()=>{
+  console.log(`伺服器啟動了`);
+})
