f7c83b83f73a17577fe68af10052a69297cd9de5
leechinghua <leechenghua107@gmail.com>
2024-06-10 14:35:52 +0800

    新增upload-imgs筆記

--- utils/upload-imgs.js
+++ utils/upload-imgs.js
@@ -7,10 +7,12 @@ const extMap = {
   "image/webp": ".webp",
 };
 
+// 定義一個文件過濾器函數 fileFilter，用來過濾不支持的文件類型。只有在 extMap 中存在的 MIME 類型才會被接受。
 const fileFilter = (req, file, callback) => {
   callback(null, !!extMap[file.mimetype]);
 };
 
+// destination：指定文件保存的資料夾。上傳文件會存到 public/img 資料夾。
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/img");
