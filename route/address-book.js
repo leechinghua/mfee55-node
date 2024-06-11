03710302e4d6fc5c833cc99562c7174779da7c20
leechinghua <leechenghua@gmail.com>
2024-06-11 16:39:03 +0800

    建立addressbook表格

--- route/address-book.js
+++ route/address-book.js
@@ -6,10 +6,33 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   res.locals.pageName = "ab-list";
 
+  const perPage = 20; // 每頁最多有幾筆
+  let page = +req.query.page || 1;
+  if (page < 1) {
+    return res.redirect(`?page=1`);
+  } // 轉向
   const sql = "SELECT COUNT(*) totalRows FROM address_book";
   const [[{ totalRows }]] = await db.query(sql); // 取得總筆數
-  res.json(totalRows);
-  // res.render("address-book/list");
+  let totalPages = 0;
+  let rows = [];
+  if (totalRows > 0) {
+    totalPages = Math.ceil(totalRows / perPage);
+    if (page > totalPages) {
+      return res.redirect(`?page=${totalPages}`);
+    }
+    const sql2 = `SELECT * FROM address_book LIMIT ${
+      (page - 1) * perPage
+    }, ${perPage}`;
+    [rows] = await db.query(sql2);
+  }
+  // res.json({ totalRows, totalPages, page, perPage, rows });
+  res.render("address-book/list", {
+    totalRows,
+    totalPages,
+    page,
+    perPage,
+    rows,
+  });
 });
 
 export default router;
