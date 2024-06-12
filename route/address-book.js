3191f36dba2373754c9ae953badbd1d3d0edee8e
leechinghua <leechenghua@gmail.com>
2024-06-12 13:41:21 +0800

    測試新增資料的表單 1

--- route/address-book.js
+++ route/address-book.js
@@ -1,38 +1,43 @@
 import express from "express";
 import moment from "moment-timezone";
 import db from "./../utils/connect-mysql.js";
-import { PrepareStatementInfo } from "mysql2";
+import upload from "./../utils/upload-imgs.js";
 
 const dateFormat = "YYYY-MM-DD";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.locals.pageName = "ab-list";
+const getListData = async (req) => {
   let keyword = req.query.keyword || ""; // 預設值為空字串
 
   let birthBegin = req.query.birthBegin || "";
-  let birthEnd = req,query.birthEnd || "";
+  let birthEnd = req.query.birthEnd || "";
 
   const perPage = 20; // 每頁最多有幾筆
   let page = +req.query.page || 1;
   if (page < 1) {
-    return res.redirect(`?page=1`);
+    return {
+      success: false,
+      redirect: `?page=1`,
+      info: "page 值太小",
+    };
   } // 轉向
 
   let where = ` WHERE 1 `;
   if (keyword) {
     where += ` AND 
-    \`name\` LIKE ${db.escape(`%${keyword}%`)}  
+    ( \`name\` LIKE ${db.escape(`%${keyword}%`)}  
     OR
-    \`mobile\` LIKE ${db.escape(`%${keyword}%`)} `;
+    \`mobile\` LIKE ${db.escape(`%${keyword}%`)}
+    ) 
+    `;
   }
   birthBegin = moment(birthBegin);
-  if(birthBegin.isValid()){
-    where += ` AND birthday >= '${birthBegin.format(dateFormat)}`
+  if (birthBegin.isValid()) {
+    where += ` AND birthday >= '${birthBegin.format(dateFormat)}'`;
   }
-  birthEnd= moment(birthEnd);
-  if(birthEnd.isValid()){
-    where += ` AND birthday >= '${birthEnd.format(dateFormat)}`
+  birthEnd = moment(birthEnd);
+  if (birthEnd.isValid()) {
+    where += ` AND birthday >= '${birthEnd.format(dateFormat)}'`;
   }
 
   const sql = `SELECT COUNT(*) totalRows FROM address_book ${where}`;
@@ -42,8 +47,13 @@ router.get("/", async (req, res) => {
   if (totalRows > 0) {
     totalPages = Math.ceil(totalRows / perPage);
     if (page > totalPages) {
-      return res.redirect(`?page=${totalPages}`);
+      return {
+        success: false,
+        redirect: `?page=${totalPages}`,
+        info: "page 值太大",
+      };
     }
+
     const sql2 = `SELECT * FROM address_book ${where} ORDER BY sid DESC LIMIT ${
       (page - 1) * perPage
     }, ${perPage}`;
@@ -54,14 +64,37 @@ router.get("/", async (req, res) => {
     });
   }
   // res.json({ totalRows, totalPages, page, perPage, rows });
-  res.render("address-book/list", {
+  return {
     totalRows,
     totalPages,
     page,
     perPage,
     rows,
     qs: req.query, // query string 參數
-  });
+  };
+};
+
+router.get("/", async (req, res) => {
+  res.locals.pageName = "ab-list";
+  const result = await getListData(req);
+
+  if (result.redirect) {
+    return res.redirect(result, redirect);
+  }
+  res.render("address-book/list", result);
+});
+
+router.get("/api", async (req, res) => {
+  const result = await getListData(req);
+  res.json(result);
+});
+
+router.get("/add", async (req, res) => {
+  res.render("address-book/add");
+});
+
+router.post("/add", upload.none(), async (req, res) => {
+  res.json(req.body);
 });
 
 export default router;
